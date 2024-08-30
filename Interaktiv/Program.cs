7ae50d1c782e29e1dfca677a21ed14d248b1ef8a
Saymmon Aziz <saymmon.aziz@elev.ga.ntig.se>
2024-08-30 10:53:03 +0200

    lkjhgffghj

--- Interaktiv/Program.cs
+++ Interaktiv/Program.cs
@@ -1,4 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello World!");
+﻿Console.WriteLine("Användarnamn: ");
+string name = Console.ReadLine();
+
+if (name == "micke")
+{
+    Console.WriteLine("Inloggad! Välkommen!");
+}
+else
+{
+    Console.WriteLine("No.");
+}
 
 Console.ReadLine();
